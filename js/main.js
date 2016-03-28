b673018b0859ee5a816f2206ebd91b1c5d469ad5
Flondor Stefan <flondorel@gmail.com>
2016-03-28 12:57:06 +0300

    get dynamic provides categories

--- js/main.js
+++ js/main.js
@@ -8,6 +8,19 @@ var helper = {
   logOut: function(){
     localStorage.removeItem('userToken');
     window.open('index.html', '_self', 'location=yes');
+  },
+  open_ntermin3page: function(categ_id){
+    window.open('ntermin3.html?categ_id='+categ_id, '_self', 'location=yes');
+  },
+  getParameterByName: function(name, url){
+    if (!url) url = window.location.href;
+    url = url.toLowerCase(); // This is just to avoid case sensitiveness
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
   }
 };
 var service = {
@@ -164,5 +177,31 @@ var service = {
         );
       }
     });
+  },
+  getAllCategories: function(){
+    $.ajax({
+      url: 'http://medserv.duk-tech.com/WS/Service.svc/GetAllCategories',
+      type: 'GET',
+      success: function (data) {
+        if (data.Status == 1) {
+          var html = '';
+          $.each(data.SearchCategory,function(index,elem){
+            console.log(elem);
+            html += '<div class="col-xs-6 "><button class="btn-default btn-choice" onclick="helper.open_ntermin3page('+elem.Id+');"><img src="img/mficon.png">'+elem.DefaultName+'</button></div>';
+          });
+          $('.categories_container').append(html);
+        }
+
+      },
+      error: function (err) {
+        console.log(err);
+        navigator.notification.alert(
+          'Error',  // message
+          function(){},         // callback
+          'Warning',            // title
+          'Ok'                  // buttonName
+        );
+      }
+  });
   }
 };
